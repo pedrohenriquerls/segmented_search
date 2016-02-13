92926a019ab06c2bcd839ef3ef4b2977ed6e6919
Pedro Henrique Rodrigues <pedrohenriquerls@gmail.com>
2016-02-13 09:33:52 -0200

    rspec dependence and database config change

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
+
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+require 'database_cleaner'
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl'
